c189379663780475e251f20d100c816a570609f3
Tarciso <tarcisoloiola@hotmail.com>
2017-11-09 21:47:50 -0500

    'commit'

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var bodyParser = require("body-parser");
 // var path = require("path");
 
 var app = express();
-var PORT = 3000;// process.env.PORT || 
+var PORT =  process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
